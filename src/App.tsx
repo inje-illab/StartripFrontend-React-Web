cd10b26b89333a9557026197943f628491781df0
Younggun Park <ygpark0922@gmail.com>
2022-03-27 22:33:48 +0900

    feat: recoil 사용 테스트 및 axios 사용 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Main from './components/Main';
+import Recoiltest from './components/Recoiltest'
 
 function App() {
   return (
     <div className="App">
         <h1>스타-링크</h1>
         <Main />
+        <Recoiltest />
     </div>
   );
 }
--- src/lib/api/NoticeAPI.tsx
+++ src/lib/api/NoticeAPI.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import axios from 'axios';
 
-function NoticeAPI(){
-
+function postNoticeAPI(){
+    // POST 요청 전송
+    axios({
+        method: 'post',
+        url: '/notice',
+        data: {
+            title: 'hello',
+        }
+    });
 }
