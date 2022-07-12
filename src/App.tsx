bc8690e730f0f7b31c86934f65cff102040e48f6
Younggun Park <ygpark0922@gmail.com>
2022-07-12 21:09:37 +0900

    feat: router add

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import Main from 'pages/Main';
 import {Container} from '@mui/material';
 import * as TestAPI from 'lib/api/TestAPI';
 import { ST_Footer, ST_Header } from 'layout';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const api = TestAPI.testAPI();
   console.log(api);
   return (
-    <>
+    <BrowserRouter>
       <ST_Header />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
       <ST_Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
